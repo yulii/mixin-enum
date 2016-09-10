db9acc2bcc6647b79f5bcf1774876dfeee14091d
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2016-09-10 14:13:21 +0900

    fix spec

--- spec/mixin/enum_spec.rb
+++ spec/mixin/enum_spec.rb
@@ -1,11 +1,5 @@
-require 'spec_helper'
-
 describe Mixin::Enum do
   it 'has a version number' do
     expect(Mixin::Enum::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
