4dbc29d59c172b535b2a16a7c263af0e86f689cd
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2016-09-10 13:44:57 +0900

    bundle gem

---
+++ lib/mixin/enum.rb
@@ -0,0 +1,7 @@
+require "mixin/enum/version"
+
+module Mixin
+  module Enum
+    # Your code goes here...
+  end
+end
---
+++ spec/mixin/enum_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Mixin::Enum do
+  it 'has a version number' do
+    expect(Mixin::Enum::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
